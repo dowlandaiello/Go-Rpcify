25611401c29c76c3c33b7ca3c99d40a075f3db5a
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-11 21:15:26 -0400

    Backup.

--- types/environment.go
+++ types/environment.go
@@ -3,3 +3,10 @@ package types
 // Environment - stack-based container capable of holding memory-only runtime values
 type Environment struct {
 }
+
+// NewEnvironment - initialize and return new instance of Environment struct
+func NewEnvironment() *Environment { // TODO: finished initializer
+	env := Environment{}
+
+	return &env
+}
