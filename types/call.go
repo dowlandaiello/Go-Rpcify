1bff601c48135c280186fee8948fc0aa8b8963d7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-12 21:02:02 -0400

    Added call initializer.

--- types/call.go
+++ types/call.go
@@ -1,13 +1,28 @@
 package types
 
+import (
+	"errors"
+	"reflect"
+)
+
 // Call - call to specified method
 type Call struct {
-	method func()
+	method     func()
+	methodHash string
 }
 
 // NewCall - initialize new instance of Call struct
-func NewCall(method func()) *Call {
-	call := Call{method}
+func NewCall(method func()) (*Call, error) {
+	if reflect.ValueOf(method).IsNil() { // Check for nil method
+		return &Call{}, errors.New("nil call") // Return error
+	}
 
-	return &call
+	call := Call{method, ""} // Init call
+
+	return &call, nil // Return initialized call instance
+}
+
+// Run - attempt to run specified call
+func (*Call) Run() error {
+	return nil
 }
