c315f5d4b31c83b50c507b648444c2f483c8a8c7
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-12 20:55:55 -0400

    Added call initializer.

--- types/call.go
+++ types/call.go
@@ -2,4 +2,12 @@ package types
 
 // Call - call to specified method
 type Call struct {
+	method func()
+}
+
+// NewCall - initialize new instance of Call struct
+func NewCall(method func()) *Call {
+	call := Call{method}
+
+	return &call
 }
