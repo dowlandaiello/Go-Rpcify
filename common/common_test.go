8f89e9765e593f75cd6b02f543da281687d44ede
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-13 13:13:50 -0400

    Added SHA unit testing.

---
+++ common/common_test.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"errors"
+	"testing"
+)
+
+// TestSHA3String - test functionality of SHA3String method
+func TestSHA3String(t *testing.T) {
+	test := SHA3String([]byte("test")) // Hash test string
+
+	if test != "NvAoWAuwLMgnKpoCD0IA40bidq5mTkXugHRVdOL1q4A=" { // Check for matching hash
+		t.Errorf(errors.New("invalid hash").Error()) // Log found error
+		t.FailNow()                                  // Panic
+	}
+
+	t.Logf("found hash %s", test) // Log success
+}
+
+// TestSHA3Bytes - test functionality of SHA3Bytes method
+func TestSHA3Bytes(t *testing.T) {
+	test := SHA3Bytes([]byte("test")) // Hash test string
+
+	if test == nil { // Check for nil hash
+		t.Errorf(errors.New("invalid hash").Error()) // Log found error
+		t.FailNow()                                  // Panic
+	}
+
+	t.Logf("found hash %b", test) // Log success
+}
