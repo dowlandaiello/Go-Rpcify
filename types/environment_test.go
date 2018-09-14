e09144550a162bf5d44fb408023c157dcdf0c975
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-14 13:32:58 -0400

    Added query unit testing.

--- types/environment_test.go
+++ types/environment_test.go
@@ -84,4 +84,79 @@ func TestAddCall(t *testing.T) {
 	t.Logf("added call %v successfully", call) // Log success
 }
 
+// TestSearchCallEndpoint - test functionality of SearchCallEndpoint method
+func TestSearchCallEndpoint(t *testing.T) {
+	env := NewEnvironment("test") // Initialize new environment
+
+	if reflect.ValueOf(env).IsNil() { // Check for nil env
+		t.Errorf(errors.New("nil environment").Error()) // Log found error
+		t.FailNow()                                     // Panic
+	}
+
+	call, err := NewCall(callTestMethod, "") // Init call
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	err = env.AddCall(call) // Attempt to add call
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	foundCall, err := env.SearchCallEndpoint(call.Endpoint) // Query for endpoint
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	t.Logf("found call %p", foundCall) // Log success
+}
+
+// TestSearchStackEndpoint - test functionality of SearchStackEndpoint method
+func TestSearchStackEndpoint(t *testing.T) {
+	env := NewEnvironment("test") // Initialize new environment
+
+	if reflect.ValueOf(env).IsNil() { // Check for nil env
+		t.Errorf(errors.New("nil environment").Error()) // Log found error
+		t.FailNow()                                     // Panic
+	}
+
+	call, err := NewCall(callTestMethod, "") // Init call
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	stack := NewStack("test") // Initialize stack
+
+	err = stack.AddCall(call) // Add call
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	err = env.AddStack(stack) // Attempt to add stack
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	foundStack, err := env.SearchStackEndpoints(stack.Endpoint) // Query for endpoint
+
+	if err != nil { // Check for errors
+		t.Errorf(err.Error()) // Log found error
+		t.FailNow()           // Panic
+	}
+
+	t.Logf("found stack %p", foundStack) // Log success
+}
+
 /* END EXPORTED METHODS */
