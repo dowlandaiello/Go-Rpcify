b346a3a9e8a6ef57564fdf177b4c2cb33619cf71
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-13 20:42:29 -0400

    Init stack unit testing.

--- types/stack_test.go
+++ types/stack_test.go
@@ -1,1 +1,23 @@
 package types
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+/* BEGIN EXPORTED METHODS */
+
+// TestNewStack - test functionality of stack initializer
+func TestNewStack(t *testing.T) {
+	stack := NewStack() // Initialize stack
+
+	if reflect.ValueOf(stack).IsNil() {
+		t.Errorf(errors.New("nil stack").Error()) // Log found error
+		t.FailNow()                               // Panic
+	}
+
+	t.Logf("initialized stack %p successfully", stack) // Log success
+}
+
+/* END EXPORTED METHODS */
