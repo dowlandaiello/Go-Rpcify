cc97412936874cb8b25770938c04a164b298eb5b
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-14 08:03:26 -0400

    Init server type.

--- networking/types/server.go
+++ networking/types/server.go
@@ -1,1 +1,18 @@
 package types
+
+import "github.com/mitsukomegumi/Go-Rpcify/types"
+
+// Server - networking type used for rpc server
+type Server struct {
+	Endpoint    string             `json:"endpoint"`    // Used for http access to server
+	Environment *types.Environment `json:"environment"` // Used for call access
+}
+
+// NewServer - initialize new instance of Server struct
+func NewServer(endpoint string) *Server {
+	env := types.NewEnvironment() // Initialize environment
+
+	server := Server{endpoint, env} // Initialize server
+
+	return &server // Return initialized server
+}
