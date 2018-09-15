f439f4b6c2ceae8813ad2cef3cdd8295345225c1
Mitsuko Megumi <mitsukomegumii@gmail.com>
2018-09-14 21:08:46 -0400

    Added server unit testing.

--- networking/types/server_test.go
+++ networking/types/server_test.go
@@ -1,1 +1,33 @@
 package types
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+// TestNewServer - test functionality of server initializer
+func TestNewServer(t *testing.T) {
+	server := NewServer("test") // Initialize server
+
+	if reflect.ValueOf(server).IsNil() {
+		t.Errorf(errors.New("nil server").Error()) // Log found error
+		t.FailNow()                                // Panic
+	}
+
+	t.Logf("initialized server %v", server) // Log success
+}
+
+// TestStartServer - test functionality of StartServer method
+func TestStartServer(t *testing.T) {
+	server := NewServer("test") // Initialize server
+
+	if reflect.ValueOf(server).IsNil() {
+		t.Errorf(errors.New("nil server").Error()) // Log found error
+		t.FailNow()                                // Panic
+	}
+
+	go server.StartServer() // Start server
+
+	t.Logf("started server %v", server)
+}
